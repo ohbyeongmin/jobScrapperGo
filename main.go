2b5400855b756960a1a45df601e4f3faafcd2c95
ohbyeongmin <fbottlem@gmail.com>
2021-06-08 01:44:08 +0900

    scrapper and echo server

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/labstack/echo"
+	"github.com/ohbyeongmin/jobScrapperGo/scrapper"
+)
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment("jobs.csv", "jobs.csv")
+}
+
+func main(){
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
+	
+}
